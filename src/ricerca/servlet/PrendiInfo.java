d8e3708f852b83412ab6feeee129523bbabdc18a
Pietro <dellisola.pietro@gmail.com>
2019-07-05 17:14:28 +0200

    ...

--- src/ricerca/servlet/PrendiInfo.java
+++ src/ricerca/servlet/PrendiInfo.java
@@ -40,43 +40,85 @@ public class PrendiInfo extends HttpServlet {
 		//Scarico la pagina
 		Document doc = Jsoup.connect("https://www.tuttitalia.it/"+regioneCliccata).get();
 		
-		//prendo la prima tabella dalla pagina scaricata in precedenza
+		Elements h1 = doc.getElementsByTag("h1");
+		String nomeRegione = h1.get(0).text();
+		
+		//Scarico tutte le tabelle presenti nella pagina
 		Elements tabelle = doc.getElementsByTag("table");
 		
-		/*Scaricare anche il div in alto con classe "ye"*/
-		
 		/*tabelle che servono:
-		 * class = "ut", in posizione 7 
-		 * senza classe(è in un div), in posizione 8 
+		 * tabella con classe "uj", posizione 0
+		 * class = "ut", in posizione 7 (quella con province)
+		 * senza classe(è in un div), in posizione 8 (quella con città maggiori)
 		 * */
 		
-		int i = 0;
-		for(Element t : tabelle) {
-			System.out.println("TABELLA "+i+"\n"+t);
-			i++;
+		Element tabellaTop = tabelle.get(0);
+		Elements celleTabella = tabellaTop.getElementsByTag("tr"); /*prelevo le righe della tabella*/
+		String sigla = "", urlStemma = "", nomeCapoluogo = "";
+		String riga = "";
+		
+		/*prelevo solo le informazioni che mi servono sigla, stemma e nomeCapoluogo*/
+		Element c = celleTabella.get(0);
+		Element el = (c.getElementsByTag("td")).get(2);
+		urlStemma = (el.getElementsByTag("img")).attr("src");
+		
+		c = celleTabella.get(1);
+		riga = c.text();
+		sigla = riga.substring(riga.indexOf(" ")+1);
+		
+		c = celleTabella.get(celleTabella.size()-1);
+		nomeCapoluogo = c.text();
+		nomeCapoluogo = nomeCapoluogo.substring(nomeCapoluogo.indexOf(" ")+1);
+		nomeCapoluogo = nomeCapoluogo.substring(0,nomeCapoluogo.indexOf(" "));
+		
+		/*tabella province*/
+		Element tabellaProvince = tabelle.get(7);
+		celleTabella = tabellaProvince.getElementsByTag("tr"); /*prelevo le righe della tabella*/
+		
+		String provincia = "", nomeProvincia = "", popolazione = "", numComuni = "";
+		int totaleComuni = 0, totalePopolazione = 0;
+		
+		for(int i = 1; i<celleTabella.size()-1; i++) { 
+			/*
+			 * la prima riga contiene l'intestazione della tabella che non serve
+			 * l'ultima riga contiene il totale di popolazioen e numeri comuni che posso calcolarmi*/
+		
+			c = celleTabella.get(i);
+			
+			riga = c.text();
+			provincia = riga.substring(0,2); //esempio SA
+			provincia = provincia.trim();
+			riga = riga.substring(2);
+			
+			numComuni = riga.substring(riga.lastIndexOf(" "));
+			riga = riga.substring(0, riga.lastIndexOf(" "));
+			numComuni = numComuni.trim();
+			
+			popolazione = riga.substring(riga.lastIndexOf(" "));
+			riga = riga.substring(0, riga.lastIndexOf(" "));
+			popolazione = popolazione.trim();
+			for(;popolazione.contains(".");) /*rimuovo l'ventuale punto*/
+				popolazione = popolazione.substring(0,popolazione.indexOf('.'))+
+						popolazione.substring(popolazione.indexOf('.')+1);
+			
+			nomeProvincia = riga;
+			nomeProvincia = nomeProvincia.trim();
+			
+			totalePopolazione = totalePopolazione + Integer.parseInt(popolazione);
+			totaleComuni = totaleComuni + Integer.parseInt(numComuni);
+			
 		}
 		
 		/*
-		        try {
-		            response.setContentType("text/html");
-		            PrintWriter writer = response.getWriter();
-		           
-		            writer.println(p);
-		            System.out.print("aggiungo    "+p);
-		            /* writer.println("<html><body>");
-		            writer.println("<p><u>Valori delle variabili locali al metodo" + " doPost()</u> <br />");
-		           
-		            writer.println("</body></html>");*/
-		/*            writer.close();
-		        } catch (Exception exc) {
-		            exc.printStackTrace();
-		        }
-		    
-		    }
-		   
-		*/
-		
-		
+		 * //String dati="2018_0_18_1_27_2_29_3_15_4_16_5_50_6_66_7_34_8_5_9_9_10_44_11_10";//Stringa realizzata scorrendo la lista di operazioni ritornata dalla DAO
+		 *    
+		 *    JSONObject jsonObject = new JSONObject();
+		 *
+	     *   jsonObject.put(dati, 0);
+	     *   
+	     *   response.getWriter().write(jsonObject.toString());
+		 */
+			
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
